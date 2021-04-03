49ffd23d7515ef9c989621fbbea52b9dec9d45b6
Sociable Steve <steve@madhouseminers.com>
2021-04-03 05:23:46 +0100

    Fixed directory changes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-nvm install lts
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install --lts
 npm install -g yarn prettier
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -11,13 +16,13 @@ cp -r ./.config/nvim ~/.config/nvim
 
 nvim --headless +PlugInstall +qa
 
-PWD=`pwd`
+ORIGINAL_PWD=`pwd`
 mkdir -p ~/.config/coc/extensions
 cd ~/.config/coc/extensions
 npm install coc-css coc-json coc-markdownlint coc-prettier coc-rls coc-tsserver
-cd $pwd
+cd $(echo $ORIGINAL_PWD | tr -d '\r')
 
 mkdir -p ~/.local/bin
-cp dev_tmux.sh ~/.local/bin/start-dev
+cp ./start-dev ~/.local/bin/
 
 export PATH="$HOME/.local/bin:$PATH"
